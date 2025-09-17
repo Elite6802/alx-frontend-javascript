44916264011844a4c18dea75dc4dc2ccedb5007e
Elite6802 <95652067+Elite6802@users.noreply.github.com>
2025-09-17 11:29:17 +0300

    Add files via upload

--- 0x04-TypeScript/task_1/js/main.js
+++ 0x04-TypeScript/task_1/js/main.js
@@ -18,3 +18,20 @@ var printTeacher = function (firstName, lastName) {
 // Example usage:
 console.log(printTeacher("John", "Doe")); // Outputs: J. Doe
 console.log(printTeacher("Jane", "Smith")); // Outputs: J. Smith
+var StudentClass = /** @class */ (function () {
+    function StudentClass(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    StudentClass.prototype.workOnHomework = function () {
+        return "Currently working";
+    };
+    StudentClass.prototype.displayName = function () {
+        return this.firstName;
+    };
+    return StudentClass;
+}());
+// Example usage to verify functionality:
+var student1 = new StudentClass("John", "Doe");
+console.log(student1.displayName()); // Should print "John"
+console.log(student1.workOnHomework()); // Should print "Currently working"
--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -38,3 +38,37 @@ const printTeacher: printTeacherFunction = (firstName: string, lastName: string)
 // Example usage:
 console.log(printTeacher("John", "Doe")); // Outputs: J. Doe
 console.log(printTeacher("Jane", "Smith")); // Outputs: J. Smith
+
+interface StudentConstructorInterface {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+interface StudentClassInterface {
+  firstName: string;
+  lastName: string;
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+// Example usage to verify functionality:
+const student1: StudentClass = new StudentClass("John", "Doe");
+console.log(student1.displayName()); // Should print "John"
+console.log(student1.workOnHomework()); // Should print "Currently working"
