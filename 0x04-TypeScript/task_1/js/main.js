276dbe1c7ce06d3d45e7f4e3246058553e83db8a
Elite6802 <95652067+Elite6802@users.noreply.github.com>
2025-09-17 11:36:24 +0300

    Add files via upload

--- 0x04-TypeScript/task_1/js/main.js
+++ 0x04-TypeScript/task_1/js/main.js
@@ -1,17 +1,3 @@
-var teacher3 = {
-    firstName: 'John',
-    fullTimeEmployee: false,
-    lastName: 'Doe',
-    location: 'London',
-    contract: false,
-};
-var director1 = {
-    firstName: 'John',
-    lastName: 'Doe',
-    location: 'London',
-    fullTimeEmployee: true,
-    numberOfReports: 17,
-};
 var printTeacher = function (firstName, lastName) {
     return "".concat(firstName[0], ". ").concat(lastName);
 };
@@ -33,5 +19,46 @@ var StudentClass = /** @class */ (function () {
 }());
 // Example usage to verify functionality:
 var student1 = new StudentClass("John", "Doe");
-console.log(student1.displayName()); // Should print "John"
-console.log(student1.workOnHomework()); // Should print "Currently working"
+console.log(student1.displayName()); // print "John"
+console.log(student1.workOnHomework()); // print "Currently working"
+// Implement the Director class
+var Director = /** @class */ (function () {
+    function Director() {
+    }
+    Director.prototype.workFromHome = function () {
+        return "Working from home";
+    };
+    Director.prototype.getCoffeeBreak = function () {
+        return "Getting a coffee break";
+    };
+    Director.prototype.workDirectorTasks = function () {
+        return "Getting to director tasks";
+    };
+    return Director;
+}());
+// Implement the Teacher class
+var Teacher = /** @class */ (function () {
+    function Teacher() {
+    }
+    Teacher.prototype.workFromHome = function () {
+        return "Cannot work from home";
+    };
+    Teacher.prototype.getCoffeeBreak = function () {
+        return "Cannot have a break";
+    };
+    Teacher.prototype.workTeacherTasks = function () {
+        return "Getting to work";
+    };
+    return Teacher;
+}());
+// Create the createEmployee function
+function createEmployee(salary) {
+    if (typeof salary === 'number' && salary < 500) {
+        return new Teacher();
+    }
+    return new Director();
+}
+// Test cases
+console.log(createEmployee(200));
+console.log(createEmployee(1000));
+console.log(createEmployee('$500'));
